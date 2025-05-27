32215cfc3df2464f3c592404cc70d8d1d3d598ea
AleksandrMaksimenko <123982407+meloch287@users.noreply.github.com>
2025-05-27 19:29:12 +0300

    Update main.py
    использование новой функции вместо параллельной отправки

--- scripts/main.py
+++ scripts/main.py
@@ -5,7 +5,7 @@ import numpy as np
 from .db_config import initialize_db, close_db_pool
 from .user_generator import generate_users
 from .transaction_generator import generate_transactions, generate_anomaly_times
-from .transaction_sender import send_transactions_parallel
+from .transaction_sender import send_transactions_parallel, send_transactions_with_timing
 from .visualization import plot_expected_load, plot_actual_load
 import logging
 
@@ -18,31 +18,30 @@ logging.basicConfig(
 
 # Параметры
 NUM_USERS = 100
-NUM_TRANSACTIONS = 30000
+NUM_TRANSACTIONS = 1000
 
 def main():
     """Основной скрипт для генерации и отправки транзакций."""
     try:
-        # Инициализация базы данных
+
         initialize_db()
 
-        # Генерация пользователей
         users = generate_users(NUM_USERS)
 
-        # Генерация временных меток и аномалий
         send_hours, send_times, anomaly_windows, combined_density, hours = generate_anomaly_times(NUM_TRANSACTIONS)
 
-        # Генерация транзакций
         payloads, bad_transaction_hours, good_transaction_hours, bad_transaction_count = generate_transactions(users, NUM_TRANSACTIONS, send_hours, anomaly_windows)
 
-        # Визуализация ожидаемой нагрузки
+
         plot_expected_load(hours, combined_density, anomaly_windows)
 
-        # Отправка транзакций
-        print("Начало отправки транзакций...")
-        logging.info("Начало отправки транзакций")
+        print("Начало отправки транзакций с учетом временных меток...")
+        logging.info("Начало отправки транзакций с учетом временных меток")
         start_time = time.time()
-        successful, failed = send_transactions_parallel(payloads, max_workers=10)
+        
+        # Используем новую функцию отправки с учетом временных меток вместо параллельной отправки
+        successful, failed = send_transactions_with_timing(payloads, send_times)
+        
         end_time = time.time()
         total_simulation_time = send_times[-1]
         logging.info(f"Отправка завершена. Время выполнения: {end_time - start_time:.2f} секунд")
@@ -50,10 +49,8 @@ def main():
         logging.info(f"Общее время симуляции: {total_simulation_time:.2f} секунд")
         print(f"Общее время симуляции: {total_simulation_time:.2f} секунд")
 
-        # Визуализация фактической нагрузки
         plot_actual_load(send_times, total_simulation_time, bad_transaction_hours, good_transaction_hours, combined_density, hours, anomaly_windows)
 
-        # Проверка первых 5 JSON-файлов
         print("\nПроверка первых 5 JSON-файлов:")
         for n in range(1, min(6, NUM_TRANSACTIONS + 1)):
             file_path = os.path.join('generated_transactions', f'txn_{n:06d}.json')
